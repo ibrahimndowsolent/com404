49c9d9f6969af4dceb43b839f766d087e831d26c
ibrahimndowsolent <4ndowi79@solent.ac.uk>
2019-09-25 11:31:33 +0000

    robot eyes

--- 1-basics/1-output/4-ascii-art/bot.py
+++ 1-basics/1-output/4-ascii-art/bot.py
@@ -1,12 +1,23 @@
 # Display Robot
+e= input("Enter into E")
+
+
+
+
+
+
+print("##########")
+print("# "+e+" "+e+" #")
+print("#  ----  #\")
+print("##########\")
+print("###########\")
+print("#\")      (\"#")
+print("#\")      (\"#")
 print("###########")
-print("#")      ("#")
-print("#")      ("#")
+print("#\")      (\"#")
+print("#\")      (\"#")
 print("###########")
-print("#")      ("#")
-print("#")      ("#")
-print("###########")
-print("#")      ("#")
-print("#")      ("#")
-print("#")      ("#")
-print("#")      ("#")
+print("#\")      (\"#")
+print("#\")      (\"#")
+print("#\")      (\"#")
+print("#\")      (\"#")
