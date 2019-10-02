f45bcb8935dd304c06ec273ab6b6f712ca828101
ibrahimndowsolent <4ndowi79@solent.ac.uk>
2019-10-02 09:55:02 +0000

    smallestnumber

---
+++ 1-basics/1-output/4-ascii-art/1-basics/3-decision/3-if-elif-else/1-basics/3-decision/3-if-elif-else/1-basics/3-decision/4-modulo-operator/1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,6 @@
+firstnumber=int(input("Please enter the first number"))
+secondnumber=int(input("Please enter the second number"))
+if firstnumber >= secondnumber:
+    print("secondnumber is the smallest")
+if secondnumber >= firstnumber:
+    print("firstnumber is the smallest")
