eeefe2fb7ba31d9016be70292789fb0d67d23446
ibrahimndowsolent <4ndowi79@solent.ac.uk>
2019-10-02 09:27:09 +0000

    paint directions/home/gitpod/.pyenv/versions/3.7.4/bin/python3

---
+++ 1-basics/1-output/4-ascii-art/1-basics/3-decision/3-if-elif-else/1-basics/3-decision/3-if-elif-else/bot.py
@@ -0,0 +1,5 @@
+directions = input ("which direction should i paint?")
+if directions == "up":
+    print ("I am painting in the upward direction!")
+    elif directions == "down":
+        print("I am painting in the down direction!")
