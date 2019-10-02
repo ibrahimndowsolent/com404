515a78e7cae8df09c3ab9db30d40b20931e90c2c
ibrahimndowsolent <4ndowi79@solent.ac.uk>
2019-10-02 11:20:17 +0000

    adventure

---
+++ 1-basics/1-output/4-ascii-art/1-basics/3-decision/3-if-elif-else/1-basics/3-decision/3-if-elif-else/1-basics/3-decision/4-modulo-operator/1-basics/3-decision/5-comparison-operators/1-basics/3-decision/7-nested-decision/bot.py
@@ -0,0 +1,7 @@
+cover=input ("what kind of book cover do you have?")
+input ("is the book perfect bound?")
+yes=input()
+if yes == "yes":
+    print("soft cover, perfect bound book are very popular!")
+
+
